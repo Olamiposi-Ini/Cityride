77cd1b8bb0a857319f8dd5fb8bb9fa13f854f7c5
Olamiposi-Ini <93973234+Olamiposi-Ini@users.noreply.github.com>
2026-06-26 07:28:29 +0100

    Delete Frontend directory

--- Frontend/android/app/src/main/kotlin/com/example/cityride/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.cityride
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- Frontend/lib/constants/api.dart
+++
@@ -1,8 +0,0 @@
-class ApiConstants {
-  static const String baseUrl = "https://cityride-backend.onrender.com";
-
-  static const String register = "$baseUrl/auth/register";
-  static const String login = "$baseUrl/auth/login";
-  static const String verifyEmail = "$baseUrl/auth/verify-email";
-  static const String resendCode = "$baseUrl/auth/resend-verification-code";
-}
